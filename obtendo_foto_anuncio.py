4eac20642e891db03d33b6a1ad18689e31898be1
rogeriodelphi <rogeriodelphi@gmail.com>
2021-12-27 23:31:34 -0300

    Obter o valor de um atributo

--- obtendo_foto_anuncio.py
+++ obtendo_foto_anuncio.py
@@ -1,13 +1,26 @@
 from IPython.core.display import display, HTML
 from capturar_dados_anuncio import anuncio
+from urllib.request import urlretrieve
 
-image = anuncio.find('div', 'image-card').img
+image = anuncio.find('div', {'class':'image-card'}).img
+# image
+
+
+# Obter o valor de um atributo
 image.get('src')
+
+#Exibindo um link clicável
+print(image.get('src'))
 
 # Visualizando a FOTO no notebook (extra)
 display(HTML(str(anuncio.find('div', {'class': 'image-card'}).img)))
 display(HTML("<img src=" + anuncio.find('div', {'class': 'image-card'}).img.get('src') + ">"))
 print(image.get('src').split('/')[-1]) #pegando o endereço da imagem
 
-from urllib.request import urlretrieve
+#Rotina para acessar e salvar a FOTO do anúncio
+#Pegando o nome da imagem
+print('Nome da imagem: ' + image.get('src').split('/')[-1]) #pegando o endereço da imagem
+
+#A função urlretrieve(url, filename) é utilizada para copiar um objeto de rede, indicado por uma URL, para um arquivo local.
+#A função urlretrieve(url, filename) precisa de dois argumentos para copiar os dados para um arquivo local.
 urlretrieve(image.get('src'), 'C:\Projetos\Web Scraping\output\img/' + image.get('src').split('/')[-1])
